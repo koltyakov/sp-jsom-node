c4af79bdc85a72005a4bd00d4dba826fc084ba1a
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2017-06-03 22:57:58 +0300

    UPS example

--- examples/basic/index.ts
+++ examples/basic/index.ts
@@ -12,11 +12,36 @@ import { JsomNode } from 'sp-jsom-node';
 
     const ctx = SP.ClientContext.get_current();
     const oWeb = ctx.get_web();
+    const peopleManager = new SP.UserProfiles.PeopleManager(ctx);
+
+    let oUsers = oWeb.get_siteUsers();
 
     ctx.load(oWeb);
+    ctx.load(oUsers);
 
     ctx.executeQueryAsync(() => {
-        console.log(oWeb);
+
+        console.log(oWeb.get_title());
+
+        let usersData = oUsers.get_data().map(user => {
+            let manager = <any>peopleManager.getUserProfilePropertyFor(user.get_loginName(), 'Manager');
+            return { user, manager };
+        });
+        ctx.executeQueryAsync(() => {
+            console.log(
+                usersData
+                    .filter(u => u.user.get_principalType() === 1) // Users
+                    .map(u => {
+                        return u.manager.get_value() ?
+                            `${u.user.get_title()}'s manager is ${
+                                usersData.filter(m => m.user.get_loginName() === u.manager.get_value())
+                                    [0].user.get_title()
+                            }` :
+                            `${u.user.get_title()} is a free man!`;
+                    }).join('\n')
+            );
+        }, errCallback);
+
     }, errCallback);
 
 
