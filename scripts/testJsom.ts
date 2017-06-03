2a4d6e4c8f32db774c08d6318629df0c35d103bc
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2017-06-03 17:33:22 +0300

    Light refactoring

--- scripts/testJsom.ts
+++ scripts/testJsom.ts
@@ -2,50 +2,32 @@ import { JsomNode, IJsomNodeSettings } from '../src';
 
 declare let global: any;
 
-let settings: any = require('../config/integration/private.2013.json');
+let settings: any = require('../config/integration/private.2016.json');
 
 console.log(settings.siteUrl);
 
-let jsom = new JsomNode({
+(new JsomNode({
     siteUrl: settings.siteUrl,
     authOptions: {
         ...(settings as any)
-    },
-    envCode: '15'
+    }
+})).init();
+
+/////
+
+// let ctx = SP.ClientContext.get_current();
+let ctx = new SP.ClientContext(`/d/gexpo/pnp`);
+
+let oWeb = ctx.get_web();
+let oLists = oWeb.get_lists();
+
+ctx.load(oWeb);
+ctx.load(oLists);
+
+ctx.executeQueryAsync(() => {
+    console.log(oWeb.get_title());
+    console.log(oLists.get_data().map(list => list.get_title()));
+}, (sender, args) => {
+    console.log(`Error: ${args.get_message()}`);
+    console.log(args.get_stackTrace());
 });
-
-jsom.createRequestClient()
-    .then((context) => {
-
-        let ctx = SP.ClientContext.get_current();
-
-        // ctx.get_webRequestExecutorFactory
-        // ctx.add_executingWebRequest((sender, e) => {
-        //     let headers = e.get_webRequest().get_headers();
-        //     for (let header of Object.keys(context.headers)) {
-        //         if (['Cookie', 'Authorization'].indexOf(header) !== -1) {
-        //             headers[header] = context.headers[header];
-        //             // headers['X-RequestDigest'] = context.digest;
-        //             // console.log(headers);
-        //         }
-        //     }
-        //     console.log(e.get_webRequest().get_url());
-        // });
-
-        let oWeb = ctx.get_web();
-        let oLists = oWeb.get_lists();
-
-        ctx.load(oWeb);
-        ctx.load(oLists);
-
-        ctx.executeQueryAsync(() => {
-            console.log(oWeb.get_title());
-            console.log(oLists.get_data().length);
-        }, (sender, args) => {
-            console.log(`Error: ${args.get_message()}`);
-            console.log(args.get_stackTrace());
-        });
-
-
-    })
-    .catch(console.log);
--- src/JsomNode.ts
+++ src/JsomNode.ts
@@ -1,11 +1,7 @@
-import * as spauth from 'node-sp-auth';
 import * as spRequest from 'sp-request';
-import * as https from 'https';
 import * as path from 'path';
 
 import { AuthConfig as SPAuthConfigirator } from 'node-sp-auth-config';
-
-import { XMLHttpRequest } from 'xmlhttprequest';
 
 import { Cpass } from 'cpass';
 
@@ -22,54 +18,20 @@ export class JsomNode {
 
     private settings: IJsomNodeSettings;
     private request: spRequest.ISPRequest;
-    private headers: any;
-    private digest: string;
 
     constructor(settings: IJsomNodeSettings) {
         this.settings = settings;
-        // this.createRequestClient();
-        this.mimicBrowserEnvironment();
-        this.loadJsomScripts(this.settings.modules, this.settings.envCode);
-        this.setDocumentProperty('URL', this.settings.siteUrl);
     }
 
-    public createRequestClient(): Promise<any> {
-        // global.XMLHttpRequest = XMLHttpRequest;
-        // XMLHttpRequest.authcookies =
-
-        let creds = {
-            ...this.settings.authOptions,
-            password:
-                (this.settings.authOptions as any).password &&
-                cpass.decode((this.settings.authOptions as any).password)
-        };
-
-        this.request = spRequest.create(creds);
-
-        return Promise.all([
-            <any>spauth.getAuth(this.settings.siteUrl, creds)
-            // spRequest.create(creds).requestDigest(this.settings.siteUrl)
-        ])
-            .then(response => {
-                this.headers = response[0].headers;
-                // this.digest = response[1];
-                return {
-                    headers: this.headers
-                    // digest: this.digest
-                };
-            });
+    public init() {
+        this.mimicBrowser();
+        this.loadScripts(this.settings.modules, this.settings.envCode);
+        this.proxyRequestManager();
+        // this.setDocumentProperty('URL', this.settings.siteUrl);
     }
 
-    private mimicBrowserEnvironment() {
-
-        // Sys$Net$XMLHttpExecutor$executeRequest
-
-        global.XMLHttpRequest = () => {
-            let invocation = new XMLHttpRequest();
-            invocation.setDisableHeaderCheck(true);
-            // request.setRequestHeader('Authorization', this.headers.Cookie || this.headers.Authorization);
-            return invocation;
-        };
+    // Mimic environment to pretend as a browser
+    private mimicBrowser() {
 
         global.navigator = {
             userAgent: 'sp-jsom-node'
@@ -85,8 +47,9 @@ export class JsomNode {
             },
             setTimeout: global.setTimeout,
             clearTimeout: global.clearTimeout,
-            // tslint:disable-next-line:no-empty
-            attachEvent: () => {},
+            attachEvent: () => {
+                //
+            },
             _spPageContextInfo: {
                 webServerRelativeUrl: `/${
                     this.settings.siteUrl
@@ -95,68 +58,58 @@ export class JsomNode {
             }
         };
 
-        // global.formdigest = {
-        //     value: this.digest, // 'DUMMY VALUE',
-        //     tagName: 'INPUT',
-        //     type: 'hidden'
-        // };
-
         global.document = {
             documentElement: {},
             URL: '',
-            // getElementsByName: (name) => {
-            //     // console.log('getElementsByName', name);
-            //     if (name === '__REQUESTDIGEST') {
-            //         return [global.formdigest];
-            //     }
-            // },
             getElementsByTagName: (name) => {
-                // console.log('getElementsByTagName', name);
                 return [];
             }
         };
 
-        // tslint:disable-next-line:no-empty
-        // global.NotifyScriptLoadedAndExecuteWaitingJobs = (scriptFileName) => {};
-
         global.Type = Function;
 
-        this.registerNamespace('Sys');
-        this.registerNamespace('SP.UI');
-        this.registerNamespace('Microsoft.SharePoint.Packaging');
+        const registerNamespace = (namespaceString: string) => {
+            let curNs = global;
+            global.window = global.window || {};
+            namespaceString.split('.').forEach(function(nsName) {
+                if (typeof curNs[nsName] === 'undefined') {
+                    curNs[nsName] = new Object();
+                }
+                curNs = curNs[nsName];
+                curNs.__namespace = true;
+            });
+            let nsName = namespaceString.split('.')[0];
+            global.window[nsName] = global[nsName];
+        };
 
-        // tslint:disable-next-line:no-empty
-        // global.RegisterModuleInit = () => {};
+        registerNamespace('Sys');
+        registerNamespace('SP.UI');
+        registerNamespace('Microsoft.SharePoint.Packaging');
 
     }
 
-    private loadJsomScripts(modules: string[] = ['core'], envCode: string = 'spo') {
+    // Load JSOM scripts to global context
+    private loadScripts(modules: string[] = ['core'], envCode: string = 'spo') {
+        global.loadedJsomScripts = global.loadedJsomScripts || [];
         modules.forEach((module: string) => {
             JsomModules[module].forEach(jsomScript => {
-                let filePath: string = path.join(__dirname, '..', 'jsom', envCode, jsomScript.replace('{{lcid}}', lcid));
-                require(filePath);
-                if (jsomScript === 'msajaxbundle.debug.js') {
-                    this.patchMsAjax();
+                if (global.loadedJsomScripts.indexOf(jsomScript) === -1) {
+                    let filePath: string = path.join(
+                        __dirname, '..', 'jsom', envCode,
+                        jsomScript.replace('{{lcid}}', lcid)
+                    );
+                    require(filePath);
+                    if (jsomScript === 'msajaxbundle.debug.js') {
+                        this.patchMicrosoftAjax();
+                    }
+                    global.loadedJsomScripts.push(jsomScript);
                 }
             });
         });
     }
 
-    private registerNamespace(namespaceString: string) {
-        let curNs = global;
-        global.window = global.window || {};
-        namespaceString.split('.').forEach(function(nsName) {
-            if (typeof curNs[nsName] === 'undefined') {
-                curNs[nsName] = new Object();
-            }
-            curNs = curNs[nsName];
-            curNs.__namespace = true;
-        });
-        let nsName = namespaceString.split('.')[0];
-        global.window[nsName] = global[nsName];
-    }
-
-    private patchMsAjax() {
+    // Escape Microsoft Ajax issues
+    private patchMicrosoftAjax() {
         let origRegisterInterface = Type.prototype.registerInterface;
         Type.prototype.registerInterface = function(typeName) {
             if (['IEnumerator', 'IEnumerable', 'IDisposable'].indexOf(typeName) !== -1) {
@@ -170,83 +123,84 @@ export class JsomNode {
             }
             return origRegisterInterface.apply(this, [].slice.call(arguments));
         };
+    }
+
+    // Proxy JSOM XmlHttpRequest through sp-request
+    private proxyRequestManager() {
+        this.request = spRequest.create({
+            ...this.settings.authOptions,
+            password:
+                (this.settings.authOptions as any).password &&
+                cpass.decode((this.settings.authOptions as any).password)
+        });
 
         (<any>Sys.Net)._WebRequestManager.prototype
             .executeRequest = (wReq: any) => {
 
-                let isJsom: boolean = wReq._url.indexOf('/_vti_bin/client.svc/ProcessQuery') !== -1;
+                let requestUrl = `${
+                    this.settings.siteUrl
+                        .replace('://', '___')
+                        .split('/')[0]
+                        .replace('___', '://')
+                    }${wReq._url}`;
 
-                let jsomHeaders = {};
-                if (isJsom) {
-                    jsomHeaders = {
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'X-RequestDigest': this.digest,
-                        'Content-Length': (wReq._body || '').length
-                    };
-                }
+                let webAbsoluteUrl = requestUrl
+                    .split('/_api')[0]
+                    .split('/_vti_bin')[0];
 
-                if (wReq._httpVerb.toLowerCase() === 'post') {
-                    this.request.post({
-                        url: `${
-                            this.settings.siteUrl
-                                .replace('://', '___')
-                                .split('/')[0]
-                                .replace('___', '://')
-                            }${wReq._url}`,
-                        headers: {
-                            ...wReq._headers,
-                            ...jsomHeaders
-                        },
-                        body: wReq._body,
-                        json: !isJsom
-                    })
-                        .then(response => {
+                this.request.requestDigest(webAbsoluteUrl)
+                    .then(digest => {
 
-                            let responseData = response.body;
-                            if (wReq._url.indexOf('/_api/contextinfo') !== -1) {
-                                this.digest = responseData.d.GetContextWebInformation.FormDigestValue;
-                                // FormDigestTimeoutSeconds
-                            }
+                        let isJsom: boolean = wReq._url.indexOf('/_vti_bin/client.svc/ProcessQuery') !== -1;
 
-                            if (!isJsom) {
-                                responseData = JSON.stringify(responseData);
-                            }
+                        let jsomHeaders = !isJsom ? {} : {
+                            'X-Requested-With': 'XMLHttpRequest',
+                            'X-RequestDigest': digest,
+                            'Content-Length': wReq._body && wReq._body.length
+                        };
 
-                            wReq._events._list.completed[0]({
-                                _xmlHttpRequest: {
-                                    status: 200,
-                                    responseText: responseData
+                        if (wReq._httpVerb.toLowerCase() === 'post') {
+                            return this.request.post({
+                                url: requestUrl,
+                                headers: {
+                                    ...wReq._headers,
+                                    ...jsomHeaders
                                 },
-                                get_aborted: () => {
-                                    return false;
-                                },
-                                get_timedOut: () => {
-                                    return false;
-                                },
-                                _SPError_: undefined,
-                                get_statusCode: () => {
-                                    return 200;
-                                },
-                                get_responseData: () => {
-                                    return responseData;
-                                },
-                                get_responseAvailable: () => {
-                                    return true;
-                                },
-                                getResponseHeader: (header) => {
-                                    return response.headers[header.toLowerCase()];
-                                }
+                                body: wReq._body,
+                                json: !isJsom
+                            }).then(response => {
+                                let responseData = isJsom ? response.body : JSON.stringify(response.body);
+                                wReq._events._list.completed[0]({
+                                    _xmlHttpRequest: {
+                                        status: response.statusCode,
+                                        responseText: responseData
+                                    },
+                                    get_statusCode: () => {
+                                        return response.statusCode;
+                                    },
+                                    get_responseData: () => {
+                                        return responseData;
+                                    },
+                                    getResponseHeader: (header) => {
+                                        return response.headers[header.toLowerCase()];
+                                    },
+                                    get_aborted: () => { return false; },
+                                    get_timedOut: () => { return false; },
+                                    get_responseAvailable: () => { return true; }
+                                });
                             });
-                        })
-                        .catch(console.log);
-                }
+                        }
+                    })
+                    .catch((err: any) => {
+                        throw new Error(err);
+                    });
 
             };
     }
 
-    private setDocumentProperty(prop: string, value: any) {
-        global.document = global.document || {};
-        global.document[prop] = value;
-    }
+    // private setDocumentProperty(prop: string, value: any) {
+    //     global.document = global.document || {};
+    //     global.document[prop] = value;
+    // }
 
 }
