a8bc05fade922dc872a83b1a584199af3ac844cb
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2017-06-03 23:10:23 +0300

    Basic taxonomy example

--- examples/basic/index.ts
+++ examples/basic/index.ts
@@ -16,30 +16,50 @@ import { JsomNode } from 'sp-jsom-node';
 
     let oUsers = oWeb.get_siteUsers();
 
+    const taxSession = SP.Taxonomy.TaxonomySession.getTaxonomySession(ctx);
+    const termStore = taxSession.getDefaultSiteCollectionTermStore();
+    const termGroups = termStore.get_groups();
+
     ctx.load(oWeb);
     ctx.load(oUsers);
+    ctx.load(termGroups, 'Include(Name,TermSets)');
 
     ctx.executeQueryAsync(() => {
 
-        console.log(oWeb.get_title());
+        console.log(`\n=== Web title: ${oWeb.get_title()} ===`);
+
+        console.log(`\n=== Terms groups:===\n`);
+        console.log(
+            termGroups.get_data().map(group => {
+                return `Group: ${group.get_name()}
+                    ${group.get_termSets().get_data().map(termSet => {
+                        return `- Termset: ${termSet.get_name()}`;
+                    }).join('\n')}
+                `;
+            }).join('\n').split('\n').map(l => l.trim()).join('\n')
+        );
 
         let usersData = oUsers.get_data().map(user => {
             let manager = <any>peopleManager.getUserProfilePropertyFor(user.get_loginName(), 'Manager');
             return { user, manager };
         });
+
         ctx.executeQueryAsync(() => {
+
+            console.log(`\n=== Users: ===\n`);
             console.log(
                 usersData
                     .filter(u => u.user.get_principalType() === 1) // Users
                     .map(u => {
                         return u.manager.get_value() ?
-                            `${u.user.get_title()}'s manager is ${
+                            `- ${u.user.get_title()}'s manager is ${
                                 usersData.filter(m => m.user.get_loginName() === u.manager.get_value())
                                     [0].user.get_title()
                             }` :
-                            `${u.user.get_title()} is a free man!`;
+                            `- ${u.user.get_title()} is a free man!`;
                     }).join('\n')
             );
+
         }, errCallback);
 
     }, errCallback);
