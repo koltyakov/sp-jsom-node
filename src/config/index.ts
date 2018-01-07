9067b38fbe41dfeee8400e78b66d5a1fb65b244a
Andrew Koltyakov <koltyakov@gmail.com>
2018-01-07 19:40:00 +0300

    Project server API

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,5 +14,6 @@ export const JsomModules: IJsomModule = {
   taxonomy: ['sp.taxonomy.debug.js'],
   userprofiles: ['sp.userprofiles.debug.js'],
   publishing: ['sp.publishing.debug.js'],
-  policy: ['sp.policy.debug.js']
+  policy: ['sp.policy.debug.js'],
+  project: ['PS.debug.js']
 };
