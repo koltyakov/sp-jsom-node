755fcb9fadf098e2a402eb8a6d88cdbc52699837
Andrew Koltyakov <koltyakov@gmail.com>
2018-01-08 00:15:25 +0300

    Get all projects experiment

--- test/manual/project.ts
+++ test/manual/project.ts
@@ -2,8 +2,22 @@ import { initEnvironment as init } from './utils/init';
 
 declare const PS: any;
 
-init({ modules: ['core', 'project'] }).then(async settings => {
+const config = {
+  config: {
+    configPath: './config/private.project-online.json'
+  },
+  modules: ['core', 'project']
+};
 
-  console.log(PS);
+init(config).then(async settings => {
+
+  const projContext = new PS.ProjectContext(settings.siteUrl);
+  let projects = projContext.get_projects();
+  projContext.load(projects, 'Include(Name, Id)');
+  projContext.executeQueryAsync(() => {
+    console.log(projects.get_data());
+  }, (...args) => {
+    console.log(projects.get_data().map(p => p.get_name()));
+  });
 
 }).catch(console.log);
